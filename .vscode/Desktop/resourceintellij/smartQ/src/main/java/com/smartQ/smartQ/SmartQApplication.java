a3911b55cc4c84c4590788d2d8a80bc14efd97c5
dnyaneshwar <code --wait>
2026-06-05 18:04:42 +0530

    initial project setup

---
+++ .vscode/Desktop/resourceintellij/smartQ/src/main/java/com/smartQ/smartQ/SmartQApplication.java
@@ -0,0 +1,13 @@
+package com.smartQ.smartQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartQApplication.class, args);
+	}
+
+}
---
+++ .vscode/Desktop/resourceintellij/smartQ/src/test/java/com/smartQ/smartQ/SmartQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartQ.smartQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartQApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
